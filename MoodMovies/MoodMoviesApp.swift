6dd41487f163221e098bd8c4f0af9cb61740cd0b
= <houssamabdoulwahab@gmail.com>
2026-03-08 12:31:33 +0100

    Initial Commit

---
+++ MoodMovies/MoodMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMoviesApp.swift
+//  MoodMovies
+//
+//  Created by Houssam on 08/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoodMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
